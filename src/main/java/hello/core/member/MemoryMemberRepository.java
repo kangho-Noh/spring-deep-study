d1bad326b0638b6922256be8211f7efd10e39bb6
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-08-21 17:19:24 +0900

    Add Service, Repository Interface and Implements

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    // 실무에서는 동시성 문제가 발생하므로 HashMap 대신 ConcurrentHashMap 쓴다.
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+
+}
