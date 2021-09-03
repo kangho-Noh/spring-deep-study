97335c1ff56f3c0b469991419113966dcd7a5844
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-09-03 20:50:38 +0900

    Add ComponentFilterAppConfigTest.java

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -7,8 +10,13 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
         //Configuration 클래스는 자동 등록에서 뺄거야. 기존 예제코드 때문에 쓴거임. 실무에서 안함
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+    // 수동 빈 등록이 자동 빈 등록이 충돌나는 경우 테스트
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 
 }
