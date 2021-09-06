a35f0575ecfb699a6bb10a62ad68601c21008db9
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-09-06 22:13:41 +0900

    생명주기 콜백은 @PostConstruct, @PreDestroy를 사용하자

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -17,10 +17,10 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig{
-        @Bean
+        @Bean //(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
-            networkClient.setUrl("http://hello-spring.dev");
+            networkClient.setUrl("http://lifeCycle.dev");
             return networkClient;
         }
     }
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -16,17 +17,31 @@ public class NetworkClient {
 
     //서비스 시작 시 호출
     public void connect(){
+        setUrl(url);
         System.out.println("connect: " + url);
 
     }
 
     public void call(String message) {
-        System.out.println("call: " + url + " message" + message);
+        System.out.println("call: " + url + " message: " + message);
 
     }
 
     //서비스 종료 시 호출
     public void disconnect(){
         System.out.println("close " + url);
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("NetworkClient.init()");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public  void close(){
+        System.out.println("NetworkClient.close()");
+        disconnect();
     }
 }
