1017156f183eb8d5dc42fb239e0f40dd973beb54
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-09-08 23:04:10 +0900

    웹 스코프와 프록시

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,13 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
+        System.out.println("myLogger = " + myLogger.getClass());
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject(); //이 시점에 만들어짐 ->init()
+        //MyLogger myLogger = myLoggerProvider.getObject(); //이 시점에 만들어짐 ->init()
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
